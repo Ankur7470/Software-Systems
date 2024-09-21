694a134a5c25d80596934967e87de03079036bb5
Ankur7470 <ankur.sinha2019@gmail.com>
2024-09-21 16:09:20 +0530

    Added Program 18

--- HandsonList-2/18.c
+++ HandsonList-2/18.c
@@ -11,23 +11,16 @@ Date : 18th Sep, 2024
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h> 
+#include <fcntl.h>
+#include <sys/wait.h>
 
 int main() {
-    int pipe1[2]; 
-    int pipe2[2]; 
+    int pipe1[2], pipe2[2];
     int pid1, pid2;
 
-    if(pipe(pipe1) == -1){
-        perror("pipe1");
-        exit(1);
-    }
+    pipe(pipe1);
 
-    if(pipe(pipe2) == -1){
-        perror("pipe2");
-        exit(1);
-    }
+    pipe(pipe2);
 
     pid1 = fork();
     if(pid1 == -1){
@@ -35,52 +28,54 @@ int main() {
         exit(1);
     }
 
-    if(pid1 == 0){
-        close(pipe1[0]);  
-        close(pipe2[0]);
-        close(pipe2[1]);  
-        close(STDOUT_FILENO);
+    if(pid1 == 0){ 
+        close(pipe1[0]);
+        dup2(pipe1[1], STDOUT_FILENO); 
+        close(pipe1[1]); 
 
-        dup2(pipe1[1], STDOUT_FILENO);
-
-        execl("/usr/bin/ls", "ls -l", "-l", NULL);
+        execlp("ls", "ls", "-l", (char *)NULL); 
+        perror("execlp ls failed");
+        exit(1);
     }
 
     pid2 = fork();
-    if(pid2 == -1){
+    if (pid2 == -1) {
         perror("fork");
         exit(1);
     }
 
-    if(pid2 == 0){
+    if(pid2 == 0){ 
         close(pipe1[1]); 
+        dup2(pipe1[0], STDIN_FILENO); 
+        close(pipe1[0]); 
+
         close(pipe2[0]); 
+        dup2(pipe2[1], STDOUT_FILENO); 
+        close(pipe2[1]); 
 
-        close(STDIN_FILENO);
-        dup2(pipe1[0], STDIN_FILENO);
-        close(STDOUT_FILENO);
-        dup2(pipe2[1], STDOUT_FILENO);
-
-        execl("/usr/bin/grep", "grep ^d", "^d", NULL);
+        execlp("grep", "grep", "^d", (char *)NULL); 
+        perror("execlp grep failed");
+        exit(1);
     }
 
-    close(pipe1[0]);  
-    close(pipe1[1]);
-    close(pipe2[1]);  
+    close(pipe1[0]); 
+    close(pipe1[1]); 
+    close(pipe2[1]); 
 
-    close(STDIN_FILENO);
-    dup2(pipe2[0], STDIN_FILENO);
+    dup2(pipe2[0], STDIN_FILENO); 
+    close(pipe2[0]); 
 
-    execl("/usr/bin/wc", "wc", NULL);
+    execlp("wc", "wc", "-l", (char *)NULL);
+    perror("execlp wc failed");
+    exit(1);
 
     return 0;
 }
 
-
 /*
-============================================================================
-Output:
-      1      10      69
-============================================================================
-*/
+=========================================
+Output-
 
+2
+=========================================
+*/
