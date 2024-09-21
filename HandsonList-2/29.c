7a694d606c7e9fdac1459537b54818373051c708
Ankur7470 <ankur.sinha2019@gmail.com>
2024-09-21 15:34:53 +0530

    Added Program 29

---
+++ HandsonList-2/29.c
@@ -0,0 +1,39 @@
+/*
+================================================================================
+Name : 29.c
+Author : Ankur Sinha
+Description : Write a program to remove the message queue.
+Date : 19th Sep, 2024
+================================================================================
+*/
+
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main() {
+    int key, msgid;
+
+    key = ftok(".", 'a');
+
+    msgid = msgget(key, 0664 | IPC_CREAT);
+
+    printf("Message queue ID: %d\n", msgid);
+
+    msgctl(msgid, IPC_RMID, NULL);
+
+    printf("Message queue removed successfully.\n");
+
+    return 0;
+}
+
+/*
+=========================================
+Output-
+
+Message queue ID: 0
+Message queue removed successfully.
+=========================================
+*/
+
+
