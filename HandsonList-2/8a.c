6b65d5666b6dde80377265b1b0df25e7251ec6fc
Ankur7470 <ankur.sinha2019@gmail.com>
2024-09-21 11:38:07 +0530

    Added Program 8

---
+++ HandsonList-2/8a.c
@@ -0,0 +1,35 @@
+/*
+================================================================================
+Name : 8a.c
+Author : Ankur Sinha
+Description : Write a separate program using signal system call to catch the following signals-
+a. SIGSEGV
+Date : 16th Sep, 2024
+================================================================================
+*/
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void sigsegv_handler(int sig) {
+    printf("Caught SIGSEGV: Segmentation Fault occurred!\n");
+    exit(1);
+}
+
+int main() {
+    signal(SIGSEGV, sigsegv_handler);  
+    int *ptr = NULL;
+    *ptr = 42; 
+    return 0;
+}
+
+/*
+=========================================
+Output-
+
+Caught SIGSEGV: Segmentation Fault occurred!
+=========================================
+*/
+
+
---
+++ HandsonList-2/8b.c
@@ -0,0 +1,36 @@
+/*
+================================================================================
+Name : 8b.c
+Author : Ankur Sinha
+Description : Write a separate program using signal system call to catch the following signals-
+b. SIGINT
+Date : 16th Sep, 2024
+================================================================================
+*/
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void sigint_handler(int sig) {
+    printf("\nCaught SIGINT: Interrupt from keyboard (Ctrl+C)!\n");
+    exit(1);
+}
+
+int main() {
+    signal(SIGINT, sigint_handler);  
+    printf("Press Ctrl+C to trigger SIGINT...\n");
+    while (1); 
+    return 0;
+}
+
+/*
+=========================================
+Output-
+
+Press Ctrl+C to trigger SIGINT...
+^C
+Caught SIGINT: Interrupt from keyboard (Ctrl+C)!
+=========================================
+*/
+
